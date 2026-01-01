1cc9ee6ab3ce47746491581aca70018f525dc833
Eelblitz <dm4price@gmail.com>
2026-01-01 14:10:32 +0100

    worked on database url and requirements.txt

--- nascohst_site/settings.py
+++ nascohst_site/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import os
+import dj_database_url
 
 # --------------------------------------------------
 # Base directory
@@ -99,10 +100,11 @@ TEMPLATES = [
 # --------------------------------------------------
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True,
+    )
 }
 
 
