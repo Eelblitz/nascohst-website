4f2be928dc4cd07c5b0020425658726d7909d5af
Eelblitz <dm4price@gmail.com>
2026-01-05 05:34:30 +0100

    removed cloudinary from settings.py

--- nascohst_site/settings.py
+++ nascohst_site/settings.py
@@ -1,9 +1,6 @@
 from pathlib import Path
 import os
 import dj_database_url
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
 
 # --------------------------------------------------
 # BASE DIRECTORY
@@ -175,23 +172,6 @@ STORAGES = {
 
 
 # --------------------------------------------------
-# CLOUDINARY CONFIGURATION
-# --------------------------------------------------
-
-CLOUDINARY_CLOUD_NAME = os.environ.get("CLOUDINARY_CLOUD_NAME")
-CLOUDINARY_API_KEY = os.environ.get("CLOUDINARY_API_KEY")
-CLOUDINARY_API_SECRET = os.environ.get("CLOUDINARY_API_SECRET")
-
-if not all([CLOUDINARY_CLOUD_NAME, CLOUDINARY_API_KEY, CLOUDINARY_API_SECRET]):
-    raise RuntimeError("Cloudinary environment variables are not fully set")
-
-cloudinary.config(
-    cloud_name=CLOUDINARY_CLOUD_NAME,
-    api_key=CLOUDINARY_API_KEY,
-    api_secret=CLOUDINARY_API_SECRET,
-    secure=True
-)
-
 
 # --------------------------------------------------
 # UPLOAD LIMITS
